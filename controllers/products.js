06a727c8a12c1d1943400928fb1779b46d499f3c
ruwanswarna <ruwanswarna@gmail.com>
2026-01-10 18:59:40 +0530

    products pagination with mongodb query language find().sort().select().skip().limit(), numeric filters $lt,$gt,$eq,$lte,$gte

--- controllers/products.js
+++ controllers/products.js
@@ -1,21 +1,68 @@
 import productService from "../services/products.js";
 const getProducts = async (req, res) => {
 	//throw new Error("testing async errors!!!!");
-	const { featured, company , name } = req.query;
-	const queryObj = {};
+	const { featured, company, name, sort, fields, numericFilters } = req.query;
+	const filters = {};
 	if (featured === "true") {
-		queryObj.featured = true;
+		filters.featured = true;
 	}
 	if (featured === "false") {
-		queryObj.featured = false;
+		filters.featured = false;
 	}
-    if(company){
-        queryObj.company = company; 
-    }
-    if(name){
-        queryObj.name = name; 
-    }
-	const products = await productService.getProducts(queryObj);
+	if (company) {
+		filters.company = company;
+	}
+	if (name) {
+		filters.name = { $regex: name, $options: "i" };
+	}
+	if (numericFilters) {
+		const operatorMap = {
+			">": "$gt",
+			"<": "$lt",
+			"=": "$eq",
+			">=": "$gte",
+			"<=": "$lte",
+		};
+
+		const regEx = /\b(<=|>=|=|<|>)\b/g;
+		let _filters = numericFilters.replace(regEx, (match) => {
+			return `-${operatorMap[match]}-`;
+		});
+		const options = ["price", "rating"];
+		_filters.split(",").forEach((item) => {
+			const [field, operator, value] = item.split("-");
+			if (options.includes(field)) {
+				if (filters[field]) {
+					filters[field] = {
+						...filters[field],
+						[operator]: Number(value),
+					};
+				} else {
+					filters[field] = { [operator]: Number(value) };
+				}
+				// if(!queryObject[field]){
+				//     queryObject[field]={}
+				// }
+				// queryObject[field][operator] = Number(value);
+				// If there is no shelf create the shelf first, then add stuff to the shelf
+			}
+		});
+	}
+
+	const sortList = sort ? sort.split(",").join(" ") : "";
+	const fieldList = fields ? fields.split(",").join(" ") : "";
+
+	const page = Number(req.query.page) || 1;
+	const limit = Number(req.query.limit) || 10;
+	const skip = (page - 1) * limit;
+
+	const products = await productService.getProducts(
+		filters,
+		sortList,
+		fieldList,
+		skip,
+		limit
+	);
 	res
 		.status(200)
 		.json({ success: true, data: { products, nbHits: products.length } });
--- services/products.js
+++ services/products.js
@@ -1,7 +1,33 @@
 import Product from "../models/product.js";
-const getProducts = async (queryObj) => {
-	return Product.find(queryObj);
+const getProducts = async (filters, sort, select, skip, limit) => {
+	//return Product.find(filters).sort(sort || "createdAt").select(select);
+	let query = Product.find(filters).sort(sort || "createdAt");
+	if (select) {
+		query = query.select(select);
+	}
+	if (skip) {
+		query = query.skip(skip);
+	}
+	if (limit) {
+		query = query.limit(limit);
+	}
+
+	return query; //return await query;
 };
 
 export default { getProducts };
- 
+
+// let result = Product.find(filters);
+// if(sort){
+//     result = result.sort(sort);
+// }
+// if(select){
+//     result = result.select(select);
+// }
+// if(skip){
+//     result = result.skip(skip);
+// }
+// if(select){
+//     result = result.limit(limit);
+// }
+// const products =await result;
